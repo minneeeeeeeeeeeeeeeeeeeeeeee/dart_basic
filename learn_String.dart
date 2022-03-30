e00cf9a9cc321b205e5a28b9362c97b11f4706ba
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 13:44:28 +0700

    name+age.toString

--- learn_String.dart
+++ learn_String.dart
@@ -2,7 +2,10 @@ void main(){
   var name = ' baam ' ;
   var city = ' lampang ' ;
   var age = 20 ;
+  var number = 25 ;
 
   print(name+city) ;
+  print(age+number) ;
+  print(name+age.toString()) ;
  
 }
