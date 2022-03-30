bfee24f743d75ea4ae2a8c9889070f9cefdba179
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 17:59:39 +0700

    Update learn_ternary_ope.dart

--- learn_ternary_ope.dart
+++ learn_ternary_ope.dart
@@ -1,3 +1,12 @@
-void maid(){
-  
+void main(){
+  var number1 = 100, number2 = 200 ;
+  String result ;
+
+  if(number1>number2){
+    result = " มากกว่า " ;
+  }else{
+    result = " น้อยกว่า " ;
+  }
+
+  print(result) ;
 }
