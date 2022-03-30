5386cfeb01a671b0abb74002b5ee4b5cd14ec538
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 10:30:29 +0700

    หัวข้อ Dynamic Type

---
+++ learn_DynamicType.dart
@@ -0,0 +1,11 @@
+void main(){
+  // static type 
+  var age = 20 ; //จำนวนเต็ม
+  var pi = 3.32 ;
+  var name = 'Hello welcome' ; // ข้อความ 
+  var isCheck = true ; 
+  print(age) ;
+  print(pi) ;
+  print(name) ;
+  print(isCheck) ;
+}
