370d0d594e5ea5c541eb213ac4b95c260b49d740
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 14:10:02 +0700

    บวก ลบ คูณ

--- learn_operators.dart
+++ learn_operators.dart
@@ -1,11 +1,13 @@
 void main(){
   var x = 100, y = 50; 
-  var z = x + y ;
-
+  var z = x + y ; // บวก
   print("การบวก = $x + $y = $z");
 
-  var a = x - y ;
+  var a = x - y ; // ลบ
   print("การลบ = $x - $y = $a");
+
+  var b = x * y ; // คูณ
+  print("การลบ = $x * $y = $b");
 
 
 }
