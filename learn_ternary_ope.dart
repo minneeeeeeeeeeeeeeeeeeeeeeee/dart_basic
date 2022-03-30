90b2fd7e1adcdf270f7239615429ab1a04602cb8
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 18:02:54 +0700

    แก้ให้สั้นลง

--- learn_ternary_ope.dart
+++ learn_ternary_ope.dart
@@ -2,11 +2,7 @@ void main(){
   var number1 = 100, number2 = 200 ;
   String result ;
 
-  if(number1>number2){
-    result = " มากกว่า " ;
-  }else{
-    result = " น้อยกว่า " ;
-  }
+  result = (number1>number2) ? "มากกว่า" : "น้อยกว่า" ;
 
   print(result) ;
 }
