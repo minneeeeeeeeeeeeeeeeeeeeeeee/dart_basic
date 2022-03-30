ce4200ec9edb821a881be4dd7c3e24d027576630
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 09:55:19 +0700

    ลองทำใหม่

--- hellomama.dart
+++ hellomama.dart
@@ -1,7 +1,3 @@
 void main(){
-print( " hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii ");
-print( " hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii ");
-print( " hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii ");
-print( " hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii ");
-print( " hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii ");
+print( " Hello atiwat ");
 }
