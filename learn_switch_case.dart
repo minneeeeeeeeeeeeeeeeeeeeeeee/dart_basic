372f01965a38b3275d1ba9f7aa1d115c617be49d
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 18:16:16 +0700

    เดือน

---
+++ learn_switch_case.dart
@@ -0,0 +1,14 @@
+void main(){
+  var monut = 1;
+  String name;
+
+  if(mount == 1){
+    name = "มกราคม";
+  }else if (monut == 2) {
+    name = "กุมภาพันธ์";
+  }else if (monut == 3) {
+    name = "มีนาคม";
+  }else{
+    name = "ไม่มีข้อมูล" ;
+  }
+}
