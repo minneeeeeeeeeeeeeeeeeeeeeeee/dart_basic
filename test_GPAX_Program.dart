31eea18d447bffdac92fdf36c8af5e8b9f5a605e
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 17:17:19 +0700

    โปรแกรมคำนวณเกรด

---
+++ test_GPAX_Program.dart
@@ -0,0 +1,25 @@
+void main(){
+  // โปรแกรมคำนวณเกรด 
+
+  /* 100 คะแนน 
+  80 => A 
+  70 => B
+  60 => C
+  50 => D
+  49 => F
+  */
+
+  var score = 50 ;
+
+  if(score>=80){
+    print("A") ;
+  }else if (score>=70){
+    print("B") ;
+  }else if (score>=60){
+    print("C") ;
+  }else if (score>=50){
+    print("D") ;
+  }else{
+    print("F")
+  }
+}
