c0eaaecb3e5f33ecb4ea704417d51488294413e9
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 10:35:53 +0700

    Update learn_DynamicType.dart

--- learn_DynamicType.dart
+++ learn_DynamicType.dart
@@ -1,8 +1,9 @@
 void main(){
   // dynamic type 
   var name1 = ' hello thailand ' ;
-
+      name1 = ' thai ' ;
   dynamic name2 = 'hi thai' ;
+          name2 = ' hi ' ;
 
   print(name1) ;
   print(name2) ;
