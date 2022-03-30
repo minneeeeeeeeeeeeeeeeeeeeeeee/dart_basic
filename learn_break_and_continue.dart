7948103a93ce617cda594f862d2f97b2a6490d63
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 20:12:40 +0700

    continue

--- learn_break_and_continue.dart
+++ learn_break_and_continue.dart
@@ -2,7 +2,7 @@ void main(){
 
   var number = 2 ;
   for(var count = 1; count<= 12; count++){
-    if(count == 3)break;
+    if(count == 3)continue;
     print(" $number x $count = ${number * count}");
   }
   print("จบโปรแกรม") ;
