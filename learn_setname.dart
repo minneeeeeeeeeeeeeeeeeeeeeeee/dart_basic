00161af19ddd9ce41e6abd03a210940bb2ca2450
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-03-30 11:40:32 +0700

    Update learn_setname.dart

--- learn_setname.dart
+++ learn_setname.dart
@@ -4,4 +4,12 @@ void main(){
 
   print(_final) ;
   print(_var) ;
+
+  String name = ' ken ' ;
+  String Name = ' kennnn ' ;
+  String NAME = ' basic ' ;
+
+  print(name) ;
+  print(Name) ;
+  print(NAME) ;
 }
