464c351ba598ed3488f61c46e6be23fb767ca927
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 14:38:49 +0700

    Update learn_list_for_loop.dart

--- learn_list_for_loop.dart
+++ learn_list_for_loop.dart
@@ -1,11 +1,7 @@
 void main(){
   List<int>number = [10, 20, 30, 40, 50, 60,] ;
 
-  print("สมาชิกตัวที่ 0 = ${number[0]}") ;
-  print("สมาชิกตัวที่ 1 = ${number[1]}") ;
-  print("สมาชิกตัวที่ 2 = ${number[2]}") ;
-  print("สมาชิกตัวที่ 3 = ${number[3]}") ;
-  print("สมาชิกตัวที่ 4 = ${number[4]}") ;
-  print("สมาชิกตัวที่ 5 = ${number[5]}") ;
-  print("สมาชิกตัวที่ 6 = ${number[6]}") ;
+  for(var index = 0; index<number.length; index++){
+    print("สมาชิกตัวที่ $index = ${number[index]}") ;
+  }
 }
