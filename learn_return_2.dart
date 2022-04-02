c8425a898b31f172300c03bd3033563385d06880
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 11:29:20 +0700

    var result = calculate(x) ;

---
+++ learn_return_2.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var x = 20.19 ;
+  var result = calculate(x) ;
+  print(" $x องศาเซลเซียล = $result องศาเควิน ") ;
+
+}
+
+// คำนวณอุณหภูมิ
+// ส่งค่าองศาเซลเซียส เข้าไป 
+// ส่งค่าองศาเควิร ออกมา
+
+//k=c+273
+
+calculate(num c) {
+  var k = c + 273 ;
+  return k ;
+}
