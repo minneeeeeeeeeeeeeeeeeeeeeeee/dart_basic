0978d250a09a9545c4e5c0bae89f6b444eb6efb1
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 14:20:32 +0700

    Create learn_list_pro.dart

---
+++ learn_list_pro.dart
@@ -0,0 +1,8 @@
+void main() {
+List<int>number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]; 
+var count =number.length ;
+print("จำนวนสมาชิก = $count") ;
+print("สมาชิกตัวแรก = ${number[0]}") ;
+print("สมาชิกตัวสุดท้าย = ${number[count-1]}") ;
+
+}
