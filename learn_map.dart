b74c4218f3bbc9d8c716e5c20ab5d34dfd39d5a8
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 16:17:07 +0700

    Update learn_map.dart

--- learn_map.dart
+++ learn_map.dart
@@ -1,5 +1,10 @@
 void main(){
   Map<int,int> number1 = {1:100,2:500,3:3000} ;
-
   print(number1[2]) ;
+
+  Map<String,String> color = {"Blue" : "สีน้ำเงิน", "Black" : "สีดำ" } ;
+  print(color["Black"]) ;
+
+  Map<int,String> code = {404:"Not Found",200:"OK"} ;
+  print(code[200]) ;
 }
