b9634b919bf31386bbc9224e029197806a1690b1
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 11:08:20 +0700

    print(" $number1 + $number2 = ${number1+number2}")

--- learn_show.dart
+++ learn_show.dart
@@ -1,11 +1,13 @@
 void main(){
-  show("flutter") ;
-  show("lampang") ;
-  show("dart") ;
-  show("100") ;
+  addNumber(10,5) ;
+ addNumber(1000, 500) ;
 
 }
 
 void show(var message){
   print("สวัสดีคุณ คำนนท์ $message") ;
+}
+
+void addNumber(num number1, num number2){
+  print(" $number1 + $number2 = ${number1+number2}") ;
 }
