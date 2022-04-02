ca3584bc8a33033c4e2778f489efdf9a0c493b9a
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 14:47:49 +0700

    Create learn_list_foreach.dart

---
+++ learn_list_foreach.dart
@@ -0,0 +1,12 @@
+void main(){
+  List<int>number = [40, 50, 60,] ;
+
+  //var total = 0 ;
+  //for(var index = 0 ; index<number.length ; index++){
+  //  total+=number[index] ;
+    
+ // }
+  for(var element in number){
+    print(element) ;
+  }
+}
