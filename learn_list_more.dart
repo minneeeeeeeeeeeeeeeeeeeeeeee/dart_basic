b857cb7212f9d26acab229c373c90e653c94c2ad
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 15:01:46 +0700

    Update learn_list_more.dart

--- learn_list_more.dart
+++ learn_list_more.dart
@@ -1,11 +1,9 @@
 void main(){
   List<String> color =["น้ำเงิน", "ส้ม", "ฟ้า"] ;
 
-  var newcolor = ["เขียวมะนาาว", "ชมพู", "นำตาล" ] ;
-
   color.add("ดำ") ; //เพิ่มที่ละ 1 
   color.add("ม่วง") ;
 
-  color.addAll(newcolor) ; //เพิ่มที่ละหลายค่า 
+  color.addAll(["เขียวมะนาาว", "ชมพู", "นำตาล"]) ; //เพิ่มที่ละหลายค่า 
   print(color) ;
 }
