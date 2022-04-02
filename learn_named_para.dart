5c56c9f51a950464264a1bca75d9866ee255db0c
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 13:38:09 +0700

    Update learn_named_para.dart

--- learn_named_para.dart
+++ learn_named_para.dart
@@ -1,3 +1,10 @@
 void main(){
-  
+  var name = "โมโม", city = "เชียงใหม่", age = "20" ;
+  showData(name : "น้ำ", city : "เชียงราย", age : "25") ;
+  showData(name:name, city:city, age:age) ;
+  showData(city : "ลำปาง", age : "21", name : "จีจี้") ;
+}
+
+void showData({String? name, String? city, String? age}) {
+  print(" ชื่อ = $name ที่อยู่ = $city อายุ = $age") ;
 }
