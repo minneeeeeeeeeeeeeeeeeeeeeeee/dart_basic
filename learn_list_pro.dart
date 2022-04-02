45b962aff5ce80ca8ece74f2d6b564e2170a7259
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 14:25:32 +0700

    Update learn_list_pro.dart

--- learn_list_pro.dart
+++ learn_list_pro.dart
@@ -1,6 +1,7 @@
 void main() {
 List<int>number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]; 
 number.add(120) ;
+number[1] = 150 ;
 var count =number.length ;
 print(number) ;
 print("จำนวนสมาชิก = $count") ;
