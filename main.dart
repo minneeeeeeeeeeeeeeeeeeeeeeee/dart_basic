43a91a602a8295368b957dec0099d8b3d8791702
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 17:49:44 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
+import 'Employee.dart';
 void main(){
-  /*
-  การเขียนอธิบายแบบยาว
-  */
-  print("Hello thanawut"); //แสดงข้อความทางหน้าจอ
+
+  //สร้าง object 
+  Employee emp1 = Employee() ;
+
+  print(emp1.name) ;
+  print(emp1.salary.toString()) ;
+
 }
