0e34589cbeb021f2ba00fb9a57d5931836c95eb4
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 15:17:43 +0700

    Update learn_remove_list.dart

--- learn_remove_list.dart
+++ learn_remove_list.dart
@@ -2,9 +2,11 @@ void main(){
   List<String> color =["น้ำเงิน", "ส้ม", "ฟ้า", "ม่วง", "ชมพู"] ;
   print(color);
 
-   color..remove("ฟ้า") ;
+  color..remove("ฟ้า") ;
   print(color) ;
 
+  color.removeRange(2,4) ;
+  print(color) ;
   print("**************************************************************************************************************************") ;
 
   List<int> number = [1,2,3,4,5,6] ;
