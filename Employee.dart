b7e9ca48c0e11c42a53c6e374391a0a59eb8c79b
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 18:08:09 +0700

    Update Employee.dart

--- Employee.dart
+++ Employee.dart
@@ -1,16 +1,30 @@
 class Employee{
 
   //คุณสมบัตร
-  String name = "หิวข้าวแล้ว" ;
-  double salary = 20000;
+  String _name = "หิวข้าวแล้ว" ;
+  double _salary = 20000;
 
   //การสร้าง method 
   void development(){
     print("เขียนโปรแกรมพัฒนาระบบสารสนเทศภายในบริษัท") ;
   }
 
-  void showData(){
-    print("ชื่อพนักงาน = "+name) ;
-    print("เงินเดือนพนักงาน = "+salary.toString()) ;
+  void _showData(){
+    print("ชื่อพนักงาน = "+_name) ;
+    print("เงินเดือนพนักงาน = "+_salary.toString()) ;
+  }
+
+void main(){
+
+  //สร้าง object 
+  Employee emp1 = Employee() ;
+  emp1._name = "สมหมาย" ;
+  emp1._salary = 25000 ;
+  emp1._showData() ;
+
+  Employee emp2 = Employee() ;
+  emp2._name = "สมศรี" ;
+  emp2._salary = 20000 ;
+  emp2._showData() ;
   }
 }
