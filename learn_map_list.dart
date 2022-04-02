03da9e9a9164e97953dc134d91632211e326c72b
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 16:35:09 +0700

    Update learn_map_list.dart

--- learn_map_list.dart
+++ learn_map_list.dart
@@ -1,3 +1,13 @@
 void main(){
-  
+  Map<String,String> color = {"Blue" : "สีน้ำเงิน", "Black" : "สีดำ" } ;
+  print(color) ;
+  print(color.values) ;
+
+   print('************************************************************************************') ;
+
+   var en = color.keys ;
+   print(en) ;
+   var th = color.values ;
+   print(th) ;
+
 }
