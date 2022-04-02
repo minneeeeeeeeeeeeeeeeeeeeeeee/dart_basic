f1aa51acd24a5cfd0b1437baa98c33f3fb62f339
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 16:29:06 +0700

    Update learn_list_map.dart

--- learn_list_map.dart
+++ learn_list_map.dart
@@ -4,4 +4,9 @@ print(username) ;
 Map<int,String> data = username.asMap();
 print(data) ;
 
+List<int> number1 = [1001,2002,3003] ;
+print(number1) ;
+Map<int,int> data1 = number1.asMap();
+print(data1) ;
+
 }
