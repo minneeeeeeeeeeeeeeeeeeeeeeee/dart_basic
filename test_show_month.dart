dea7dd4af0cc1f4434e749138b057e9ec488fcec
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 11:36:13 +0700

    showMonth

---
+++ test_show_month.dart
@@ -0,0 +1,16 @@
+void main(){
+
+}
+
+void showMonth(int number){
+String month ;
+switch(number){
+  case 1 : month = "มกราคม" ;
+    break;
+  case 2 : month = "กุมภาพันธ์" ;
+    break;
+  case 3 : month = "มีนาคม" ;
+    break;
+  default : month = "ไม่พบเดือน" ;
+  }
+}
