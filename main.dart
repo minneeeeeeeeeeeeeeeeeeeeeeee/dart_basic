4319c56d1b0c352b716d29b57ceb0a3c48f537f1
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 18:59:22 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,13 +3,13 @@ void main(){
 
   //สร้าง object 
   Employee emp1 = Employee("สมหมาย", 25000) ;
-  print(emp1.getSalary()) ;
-
   Employee emp2 = Employee("สมศรี", 20000) ;
-  print(emp2.getSalary()) ;
-
   Employee emp3 = Employee("สมหวัง", 20000) ;
-  print(emp3.getSalary()) ;
+  
+
+  emp1.showData() ;
+  emp2.showData() ;
+  emp3.showData() ;
   
 
 }
