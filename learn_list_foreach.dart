7fb38b674f397c050002e1ccf5a0fca5b59537c2
minne <95323929+minneeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-02 14:49:33 +0700

    Update learn_list_foreach.dart

--- learn_list_foreach.dart
+++ learn_list_foreach.dart
@@ -1,12 +1,9 @@
 void main(){
   List<int>number = [40, 50, 60,] ;
 
-  //var total = 0 ;
-  //for(var index = 0 ; index<number.length ; index++){
-  //  total+=number[index] ;
-    
- // }
+  var total = 0 ;
   for(var element in number){
-    print(element) ;
+    total+=element ;
   }
+  print('ผลรวมเท่ากับ = $total') ;
 }
